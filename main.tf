8a840bde9a3051d8756ab1fb95145c152e3947cc
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-14 01:12:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
+  version = "~> 3.0"
 }
 resource "aws_instance" "master_node" {
   ami           = local.ami
