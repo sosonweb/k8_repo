b849f1ff5bddcc7618b5bf43901736825fa8c822
Sai_Swetha <137962888+saiswethabalanagar@users.noreply.github.com>
2024-04-13 23:46:35 -0500

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type    = string
+  description = "AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type    = string
+  description = "AWS Secret Access Key"
+}
